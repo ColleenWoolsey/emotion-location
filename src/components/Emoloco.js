8f657e235ba71c8e8f3a78eff09ee5a6b3559420
Colleen Woolsey <rlwool@aol.com>
2019-02-04 13:34:03 -0600

    added Emolooco.js and Emoloco.css

---
+++ src/components/Emoloco.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import AppViews from "./AppViews";
+import "./Emoloco.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Emoloco extends Component {
+  render() {
+    return (
+        <AppViews />
+    );
+  }
+}
+
+export default Emoloco;
