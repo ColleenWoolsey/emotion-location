447f89cb99e29d51c0e92b13b424ca5ee874d0a4
Colleen Woolsey <rlwool@aol.com>
2019-02-05 10:10:35 -0600

    Adjusted {} () and <> < /> in routes

--- src/components/AppViews.js
+++ src/components/AppViews.js
@@ -2,6 +2,8 @@ import { Route, Redirect } from "react-router-dom";
 import React, { Component } from "react";
 import EmotionManager from "../modules/EmotionManager";
 import EmotionList from "./EmotionList";
+import EmotionCard from "./EmotionCard";
+import EmotionDetail from "./emotion/EmotionDetail";
 
 
 export default class AppViews extends Component {
@@ -19,12 +21,29 @@ export default class AppViews extends Component {
        })
    };
 
+
    render() {
        return(
+// This is the list of emotions
+         <React.Fragment>
+
            <Route exact path="/emotions"
            render={props => {
                return <EmotionList emotions={this.state.emotions} />            
-            }
-          } />
-        )
+            }}
+           />
+    
+{/* This is the detail for individual emotions */}
+            <Route path="/emotions/:emotionId(/d+)"
+            render={props => {
+                return (
+                    <EmotionDetail {...props}
+                    emotions={this.state.emotions} />
+                );
+            }}
+          />
+
+         </React.Fragment>
+       )
+      }
     }
